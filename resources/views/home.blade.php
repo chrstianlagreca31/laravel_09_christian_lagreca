616494707c7e1167ff325b72b85682f215109259
christian <chris06lagreca@gmail.com>
2026-01-28 14:58:28 +0100

    pasqua

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,8 @@
-@extends('layouts.app')
+<x-app>
+    <div class="text-center">
+        <h1 class="mb-3">Benvenuto nel Blog</h1>
+        <p class="lead">Crea e gestisci i tuoi post</p>
+        <a href="{{ route('posts.create') }}" class="btn btn-primary">Crea Post</a>
+    </div>
+</x-app>
 
-@section('content')
-<div class="text-center">
-    <h1 class="mb-3">Benvenuto nel Blog</h1>
-    <p class="lead">Crea e gestisci i tuoi post</p>
-    <a href="{{ route('posts.create') }}" class="btn btn-primary">Crea Post</a>
-</div>
-@endsection
